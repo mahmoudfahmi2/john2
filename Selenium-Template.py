59bcb16784864c3bdf9666f439d112cd9b4af22e
mahmoudfahmi2 <126987016+mahmoudfahmi2@users.noreply.github.com>
2023-03-04 22:56:28 +0200

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -1,35 +1,45 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
-display = Display(visible=0, size=(800, 800))  
+display = Display(visible=0, size=(2000, 2000))
 display.start()
+import undetected_chromedriver as uc
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-                                      # and if it doesn't exist, download it automatically,
-                                      # then add chromedriver to path
-
-chrome_options = webdriver.ChromeOptions()    
-# Add your options as needed    
-options = [
-  # Define window size here
-   "--window-size=1200,1200",
-    "--ignore-certificate-errors"
- 
-    #"--headless",
-    #"--disable-gpu",
-    #"--window-size=1920,1200",
-    #"--ignore-certificate-errors",
-    #"--disable-extensions",
-    #"--no-sandbox",
-    #"--disable-dev-shm-usage",
-    #'--remote-debugging-port=9222'
-]
-
-for option in options:
-    chrome_options.add_argument(option)
-
-driver = webdriver.Chrome(options = chrome_options)
-driver.get('http://github.com')
-
+options = uc.ChromeOptions()
+options.add_argument('--disable-gpu')
+options.add_argument('--load-extension=./buster')
+driver = uc.Chrome(version_main = 108 ,options=options)
+driver.set_window_size(2000, 2000)
+wait = WebDriverWait(driver, 30)
+driver.get("https://cuty.io/fetchrewards")
+try:
+  time.sleep(20)
+  wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="submit-button"]'))).click()
+  time.sleep(5)
+  wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="submit-button"]'))).click()
+  time.sleep(5)
+  wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="submit-button"]'))).click()
+  time.sleep(5)
+  wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="submit-button"]'))).click()
+  time.sleep(5)
+  wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="submit-button"]'))).click()
+except:
+  time.sleep(10)
+  driver.save_screenshot('screenie.png')
+  driver.switch_to.frame(driver.find_element(By.XPATH, ".//iframe[@title='recaptcha challenge expires in two minutes']"))
+  wait.until(EC.element_to_be_clickable((By.XPATH, '//div[2]/div/div/div[4]'))).click()
+  time.sleep(10)
+  driver.save_screenshot('screenie.png')
+  try:
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="submit-button"]'))).click()
+  except:
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//div[2]/div/div/div[4]'))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="submit-button"]'))).click()
+  time.sleep(5)
+  driver.save_screenshot('screenie.png')
